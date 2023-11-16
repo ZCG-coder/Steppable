82aaab91a59f9ce50796774993da1768948e736c
Andy Zhang <z-c-ge@outlook.com>
2023-11-16 12:30:26 +0800

    Added power function

--- include/fn/basicArithm.hpp
+++ include/fn/basicArithm.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "output.hpp"
+
+#include <exception>
 #include <string>
 #include <string_view>
 
@@ -8,3 +11,26 @@ std::string compare(const std::string_view& a, const std::string_view& b, int st
 std::string multiply(const std::string_view& a, const std::string_view& b, int steps = 2);
 std::string subtract(const std::string_view& a, const std::string_view& b, int steps = 2);
 std::string decimalConvert(const std::string& inputStr, const std::string& baseStr, int steps = 2);
+std::string power(const std::string_view _number, const std::string_view& raiseTo, const int steps = 2);
+
+template<typename Pred>
+void loop(const std::string_view& times, Pred predicate)
+{
+    std::string current = "0";
+    auto result = compare(current, times, 0);
+
+    while (result == "0")
+    {
+        try
+        {
+            predicate(current);
+        }
+        catch (std::exception e)
+        {
+            error("Exception occured in predicate.");
+            error("Exception message: {}", e.what());
+        }
+        current = add(current, "1", 0);
+        result = compare(current, times, 0);
+    }
+}
--- src/power/power.cpp
+++ src/power/power.cpp
@@ -1,7 +1,40 @@
+#include "argParse.hpp"
+#include "fn/basicArithm.hpp"
+#include "powerReport.hpp"
+#include "util.hpp"
+
 #include <iostream>
 
-auto power(const std::string_view& number, const std::string_view& raiseTo)
+std::string power(const std::string_view _number, const std::string_view& raiseTo, const int steps)
 {
-    std::string times = "0";
-    std::string out = "0";
+    std::string numberOrig = static_cast<std::string>(_number), number = "1";
+    return reportPower(_number, raiseTo, steps);
 }
+
+#ifndef NO_MAIN
+int main(const int _argc, const char* _argv[])
+{
+    UTF8CodePage _;
+    ProgramArgs program(_argc, _argv);
+    program.addPosArg('a', "Number as the base");
+    program.addPosArg('b', "Number as the exponent");
+    program.addKeywordArg(
+        "steps", 2, "Amount of steps while raising the power (i.e., multiplying). 0 = No steps, 2 = All steps.");
+    program.addSwitch("profile", false, "profiling the program");
+    program.parseArgs();
+
+    int steps = program.getKeywordArgument("steps");
+    bool profile = program.getSwitch("profile");
+    const auto& aStr = program.getPosArg(0);
+    const auto& bStr = program.getPosArg(1);
+
+    if (profile)
+    {
+        TIC(Column Method Power)
+        std::cout << "Column Method Power :\n" << power(aStr, bStr) << std::endl;
+        TOC()
+    }
+    else
+        std::cout << power(aStr, bStr, steps) << std::endl;
+}
+#endif
