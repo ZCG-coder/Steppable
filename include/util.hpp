6cf3181786cb9cd34e5b1fb1de1811457dd8880b
Andy Zhang <andy@nwsoft.tech>
2023-11-22 17:43:09 +0800

    Fixed compare and segfaults. - Fixed wrong result if argument <a> or <b> of compare contains leading zeros. - Fixed problem of removeLeadingZeros() that causes it to remove ALL zeros if the input is a string with zeros. - Fixed segmentation fault on subtract if <b> is a string with zeros. - Added copyright header to every source file.

--- include/util.hpp
+++ include/util.hpp
@@ -1,3 +1,25 @@
+/**************************************************************************************************
+ * Copyright (c) 2023 NWSOFT                                                                      *
+ *                                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy                   *
+ * of this software and associated documentation files (the "Software"), to deal                  *
+ * in the Software without restriction, including without limitation the rights                   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                      *
+ * copies of the Software, and to permit persons to whom the Software is                          *
+ * furnished to do so, subject to the following conditions:                                       *
+ *                                                                                                *
+ * The above copyright notice and this permission notice shall be included in all                 *
+ * copies or substantial portions of the Software.                                                *
+ *                                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                  *
+ * SOFTWARE.                                                                                      *
+ **************************************************************************************************/
+
 #pragma once
 
 #include "colors.hpp"
@@ -6,9 +28,9 @@
 #include <array>
 #include <chrono>
 #include <iomanip>
+#include <sstream>
 #include <string>
 #include <string_view>
-#include <sstream>
 #include <vector>
 
 #ifndef TIC
@@ -210,6 +232,8 @@ inline auto bothEndsReplace(const std::basic_string<CharT> s, const CharT t, con
 }
 
 auto removeLeadingZeros(const std::vector<int> vector) -> decltype(vector);
+
+auto removeLeadingZeros(const std::string string) -> decltype(string);
 
 std::string makeWider(const std::string& orig);
 
