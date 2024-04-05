ac3f89c2d6622049c863e256231ace5617db70ec
813ethan <129200665+813ethan@users.noreply.github.com>
2024-04-05 21:44:58 +0800

    Update number.hpp
    Add comment for enum RoundingMode
    
    Signed-off-by: 813ethan <129200665+813ethan@users.noreply.github.com>

--- include/number.hpp
+++ include/number.hpp
@@ -24,12 +24,25 @@
 
 namespace steppable
 {
+    /**
+     * @enum RoundingMode
+     * @brief Specifies how Steppable should round the number in operations.
+     */
     enum RoundingMode
     {
+        /// @brief Use the higher precision whenever possible.
         USE_MAXIMUM_PREC = 0xFF,
+
+        /// @brief Use the lower precision whenever possible.
         USE_MINIMUM_PREC = 0x01,
+
+        /// @brief Use the current precision.
         USE_CURRENT_PREC = 0x02,
+
+        /// @brief Use the other number's precision.
         USE_OTHER_PREC = 0x03,
+
+        /// @brief Do not append any decimal places.
         DISCARD_ALL_DECIMALS = 0x00
     };
 
