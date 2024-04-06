258d6156d0c98e2b94be719b38d09efbf2cf6ba0
Andy Zhang <z-c-ge@outlook.com>
2024-04-06 18:59:41 +0800

    Added default arguments.

--- lib/bindings.cpp
+++ lib/bindings.cpp
@@ -2,20 +2,55 @@
 
 #include <Python.h>
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/string_view.h>
+
+namespace nb = nanobind;
+using namespace steppable::__internals::arithmetic;
+using namespace nb::literals;
 
 NB_MODULE(bindings, mod)
 {
-    mod.doc() = "Python bindings for the Steppable library."; // optional module docstring
+    auto internals = mod.def_submodule("_internals", "Internal functions.");
 
-    mod.def("abs",
-            &steppable::__internals::arithmetic::add,
-            "Internal function that takes the absolute value of a number.");
-    mod.def("add", &steppable::__internals::arithmetic::add, "Internal function that adds two numbers.");
-    mod.def("subtract", &steppable::__internals::arithmetic::subtract, "Internal function that subtracts two numbers.");
-    mod.def("multiply", &steppable::__internals::arithmetic::multiply, "Internal function that multiplys two numbers.");
-    mod.def("divide", &steppable::__internals::arithmetic::divide, "Internal function that divides two numbers.");
-    mod.def("divideWithQuotient",
-            &steppable::__internals::arithmetic::divideWithQuotient,
-            "Internal function that divides two numbers, but separating the quotient and remainder.");
-    mod.def("power", &steppable::__internals::arithmetic::power, "Internal function that raises a number to a power.");
+    internals.def("abs",
+                  &steppable::__internals::arithmetic::abs,
+                  "a"_a,
+                  "steps"_a = 2,
+                  "Internal function that takes the absolute value of a number.");
+    internals.def(
+        "add",
+        [](const std::string& a, const std::string& b, const int steps) { return add(a, b, steps); },
+        "a"_a,
+        "b"_a,
+        "steps"_a = 2,
+        "Internal function that adds two numbers.");
+    internals.def(
+        "subtract",
+        [](const std::string& a, const std::string& b, const int steps) { return subtract(a, b, steps); },
+        "a"_a,
+        "b"_a,
+        "steps"_a = 2,
+        "Internal function that subtracts two numbers.");
+    internals.def(
+        "multiply",
+        [](const std::string& a, const std::string& b, const int steps) { return multiply(a, b, steps); },
+        "a"_a,
+        "b"_a,
+        "steps"_a = 2,
+        "Internal function that multiplies two numbers.");
+    internals.def(
+        "divide",
+        [](const std::string& a, const std::string& b, const int steps, const int decimals) {
+            return divide(a, b, steps);
+        },
+        "a"_a,
+        "b"_a,
+        "steps"_a = 2,
+        "decimals"_a = 5,
+        "Internal function that divides two numbers.");
+    internals.def("divideWithQuotient",
+                  &divideWithQuotient,
+                  "Internal function that divides two numbers, but separating the quotient and remainder.");
+    internals.def("power", &power, "a"_a, "raiseTo"_a, "steps"_a = 2, "Internal function raises a number to a power.");
 }
