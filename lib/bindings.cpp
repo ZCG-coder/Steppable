46503ff3cbe7b1f382941271ec0991b7a18f0407
Andy Zhang <z-c-ge@outlook.com>
2024-04-06 17:45:08 +0800

    Switched to wjakob/nanobind for better performance.

--- lib/bindings.cpp
+++ lib/bindings.cpp
@@ -1,12 +1,15 @@
 #include "fn/basicArithm.hpp"
 
 #include <Python.h>
-#include <pybind11/pybind11.h>
+#include <nanobind/nanobind.h>
 
-PYBIND11_MODULE(bindings, mod)
+NB_MODULE(bindings, mod)
 {
     mod.doc() = "Python bindings for the Steppable library."; // optional module docstring
 
+    mod.def("abs",
+            &steppable::__internals::arithmetic::add,
+            "Internal function that takes the absolute value of a number.");
     mod.def("add", &steppable::__internals::arithmetic::add, "Internal function that adds two numbers.");
     mod.def("subtract", &steppable::__internals::arithmetic::subtract, "Internal function that subtracts two numbers.");
     mod.def("multiply", &steppable::__internals::arithmetic::multiply, "Internal function that multiplys two numbers.");
