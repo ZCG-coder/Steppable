666c35ee0a89407aacc98226a17fa19f7dabc39b
Andy Zhang <z-c-ge@outlook.com>
2024-06-03 22:47:10 +0800

    Added `factorial`.

---
+++ src/factorial/factorialReport.cpp
@@ -0,0 +1,48 @@
+/**************************************************************************************************
+ * Copyright (c) 2023-2024 NWSOFT                                                                 *
+ *                                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy                   *
+ * of this software and associated documentation files (the "Software"), to deal                  *
+ * in the Software without restriction, including without limitation the rights                   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                      *
+ * copies of the Software, and to permit persons to whom the Software is                          *
+ * furnished to do so, subject to the following conditions:                                       *
+ *                                                                                                *
+ * The above copyright notice and this permission notice shall be included in all                 *
+ * copies or substantial portions of the Software.                                                *
+ *                                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                  *
+ * SOFTWARE.                                                                                      *
+ **************************************************************************************************/
+
+#include "factorialReport.hpp"
+
+#include "fn/basicArithm.hpp"
+#include "symbols.hpp"
+
+#include <sstream>
+#include <string>
+
+using namespace steppable::__internals::arithmetic;
+using namespace steppable::__internals::symbols;
+
+std::string reportFactorial(const std::string& number, const std::string& result, int steps)
+{
+    if (steps == 0)
+        return result;
+
+    // In this case, steps == 2 is equivalent to steps == 1.
+    std::stringstream ss;
+    loop(number, [&](const auto& item) { ss << add(item, "1", 0) << " " << MULTIPLY << " "; });
+    auto out = ss.str();
+
+    out.erase(out.end() - 3, out.end()); // Remove the last " * "
+    out += " = " + result;
+
+    return out;
+}
---
+++ src/factorial/factorialReport.hpp
@@ -0,0 +1,35 @@
+/**************************************************************************************************
+ * Copyright (c) 2023-2024 NWSOFT                                                                 *
+ *                                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy                   *
+ * of this software and associated documentation files (the "Software"), to deal                  *
+ * in the Software without restriction, including without limitation the rights                   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                      *
+ * copies of the Software, and to permit persons to whom the Software is                          *
+ * furnished to do so, subject to the following conditions:                                       *
+ *                                                                                                *
+ * The above copyright notice and this permission notice shall be included in all                 *
+ * copies or substantial portions of the Software.                                                *
+ *                                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                  *
+ * SOFTWARE.                                                                                      *
+ **************************************************************************************************/
+
+#pragma once
+
+#include <string>
+
+/**
+ * @brief Report the factorial calculation.
+ *
+ * @param number The number to calculate the factorial of.
+ * @param result The result of the factorial calculation.
+ * @param steps The number of steps taken to calculate the factorial.
+ * @return std::string The report of the factorial calculation.
+ */
+std::string reportFactorial(const std::string& number, const std::string& result, int steps);
