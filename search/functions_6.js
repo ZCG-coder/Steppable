482b307ff3f2d95c1c1e49b03c3d16114a81f2a6
ZCG-coder <z-c-ge@outlook.com>
2024-06-04 14:53:45 +0000

    Deploying to gh-pages from @ ZCG-coder/Steppable@40c4d0cdfb24e1e74b382a614155b5167dd50ebf 🚀

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['fraction_0',['Fraction',['../classsteppable_1_1Fraction.html#a1fa38ab4cede78e19fa478242f6e5b46',1,'steppable::Fraction::Fraction(const std::string &amp;top=&quot;1&quot;, const std::string &amp;bottom=&quot;1&quot;)'],['../classsteppable_1_1Fraction.html#a4647e3f3062426a91d1ef7cdbde23bb6',1,'steppable::Fraction::Fraction(const Number &amp;number)'],['../classsteppable_1_1Fraction.html#ab5b4a2bd2c9223f2ea7bc5c699857aee',1,'steppable::Fraction::Fraction()']]]
+  ['factorial_0',['factorial',['../namespacesteppable_1_1____internals_1_1arithmetic.html#a59af1f3ad78fe203eb847fde390f7742',1,'steppable::__internals::arithmetic']]],
+  ['fraction_1',['Fraction',['../classsteppable_1_1Fraction.html#a1fa38ab4cede78e19fa478242f6e5b46',1,'steppable::Fraction::Fraction(const std::string &amp;top=&quot;1&quot;, const std::string &amp;bottom=&quot;1&quot;)'],['../classsteppable_1_1Fraction.html#a4647e3f3062426a91d1ef7cdbde23bb6',1,'steppable::Fraction::Fraction(const Number &amp;number)'],['../classsteppable_1_1Fraction.html#ab5b4a2bd2c9223f2ea7bc5c699857aee',1,'steppable::Fraction::Fraction()']]]
 ];
